94e606d047e70c27938d1131e4a54b458fafb434
twojnarowski <tymoteusz1138@gmail.com>
2024-04-28 14:07:44 +0200

    Add project files.

---
+++ MauiAppOnPlatformDefaultRepro/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppOnPlatformDefaultRepro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
